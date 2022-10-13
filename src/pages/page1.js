324f3ef1ff3049577b938a379700a51b0baf03da
marcelsafont <marcel.safont1983@gmail.com>
2022-10-13 12:12:35 +0200

    first commit

---
+++ src/pages/page1.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+const Page1 = () => {
+  return (
+    <div className="main">
+      <h1> Page number one</h1>
+      <p>Testing porpuses</p>
+    </div>
+  )
+}
+
+export default Page1
+
+export const Head = () => <title>Page 1</title>
---
+++ src/pages/page2.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+const Page2 = () => {
+  return (
+    <div className="main">
+      <h1> Page number two</h1>
+      <p>New page number two</p>
+    </div>
+  )
+}
+
+export default Page2
+
+export const Head = () => <title>Page 1</title>
