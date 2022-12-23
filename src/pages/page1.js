5ec92c1c085c163e8e3ab779c249eb218a25c167
marcelsafont <marcel.safont1983@gmail.com>
2022-12-23 07:36:55 +0100

    added accesibe

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,6 +1,27 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 const Page1 = () => {
+  const [accesLoaded, setAccesLoaded] = useState(false);
+  useEffect(() => {
+    
+    if(typeof window !== 'undefined'){
+      const s = document.createElement('script'); 
+      s.src = 'https://acsbapp.com/apps/app/dist/js/app.js';
+      s.async = true; 
+      s.addEventListener('load', ()=>setAccesLoaded(true))
+      document.querySelector('head').appendChild(s);
+    }
+    
+  }, [])
+
+  useEffect(() => {
+    if(!accesLoaded) return;
+    if(typeof window !== 'undefined'){
+      window.acsbJS.init();
+    }
+  },[accesLoaded])
+
+ 
   return (
     <div className="main">
       <nav>
